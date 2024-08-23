5944f1ff4a170aedcdddf4b81cbf86d9878db086
Chad Cote <chadcote58@gmail.com>
2024-08-23 13:10:24 -0400

    Created the user view

--- mtgconnectapi/views/__init__.py
+++ mtgconnectapi/views/__init__.py
@@ -2,3 +2,4 @@ from .deck_view import DeckView
 from .comment_view import CommentView
 from .format_view import FormatView
 from .post_view import PostView
+from .auth import login_user, register_user
